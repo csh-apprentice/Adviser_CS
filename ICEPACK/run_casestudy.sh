e1e26650b1ba7fadd9640fcf10f1d26a98d38d9e
Shihan Cheng <shihanc@as-97b0vt3.vuds.vanderbilt.edu>
2025-12-22 01:48:38 -0600

    correct_output_path

--- ICEPACK/run_casestudy.sh
+++ ICEPACK/run_casestudy.sh
@@ -17,7 +17,7 @@ sudo apt-get install -y openmpi-bin
 cd casestudy
 for a in 0.3 0.5 0.7 1.0 1.3 1.6 2.0; do
   python -m experiments.run_forward \
-    --out ../adviser_output/sweep_A_${a} \
+    --out ../../../adviser_output/sweep_A_${a} \
     --fluidity-scale "$a" \
     --dx 5000
 done
@@ -25,7 +25,7 @@ done
 
 for dx in 8000 6000 5000 4000 3000; do
   python -m experiments.run_forward \
-    --out adviser_output/scale_dx_${dx} \
+    --out ../../../adviser_output/scale_dx_${dx} \
     --fluidity-scale 1.0 \
     --dx $dx
 done
@@ -43,7 +43,7 @@ done
 for np in 1 2 4 8 16; do
   mpiexec -n $np \
     python -m experiments.run_forward \
-      --out adviser_output/mpi_np_${np} \
+      --out ../../../adviser_output/mpi_np_${np} \
       --fluidity-scale 1.0 \
       --dx 4000
 done
