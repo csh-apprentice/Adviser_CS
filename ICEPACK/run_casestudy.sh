cbf311b9fe76ee1e5443595e80c354a544dba062
Shihan Cheng <shihanc@as-csbhnl3.vuds.vanderbilt.edu>
2025-12-25 19:05:11 -0600

    ICEPACK: update test_parallel_scaling.sh solve slots

--- ICEPACK/run_casestudy.sh
+++ ICEPACK/run_casestudy.sh
@@ -15,20 +15,20 @@ sudo apt-get update
 sudo apt-get install -y openmpi-bin
 
 cd casestudy
-for a in 0.3 0.5 0.7 1.0 1.3 1.6 2.0; do
-  python -m experiments.run_forward \
-    --out ../../../adviser_output/sweep_A_${a} \
-    --fluidity-scale "$a" \
-    --dx 5000
-done
+# for a in 0.3 0.5 0.7 1.0 1.3 1.6 2.0; do
+#   python -m experiments.run_forward \
+#     --out ../../../adviser_output/sweep_A_${a} \
+#     --fluidity-scale "$a" \
+#     --dx 5000
+# done
 
 
-for dx in 8000 6000 5000 4000 3000; do
-  python -m experiments.run_forward \
-    --out ../../../adviser_output/scale_dx_${dx} \
-    --fluidity-scale 1.0 \
-    --dx $dx
-done
+# for dx in 8000 6000 5000 4000 3000; do
+#   python -m experiments.run_forward \
+#     --out ../../../adviser_output/scale_dx_${dx} \
+#     --fluidity-scale 1.0 \
+#     --dx $dx
+# done
 
 
 
@@ -40,10 +40,10 @@ done
 #       --dx 4000
 # done
 
-for np in 1 2 4 8 16; do
+for np in 1 2 4 8; do
   mpiexec -n $np \
     python -m experiments.run_forward \
       --out ../../../adviser_output/mpi_np_${np} \
       --fluidity-scale 1.0 \
-      --dx 4000
+      --dx 2000
 done
